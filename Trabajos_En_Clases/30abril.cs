5c66bd2e4fd20c1090d47fd7063d5b420c791da4
AngheloZambrana <anghelozambrana2005@gmail.com>
2024-04-30 14:19:32 -0400

    Mi primer commit

---
+++ Trabajos_En_Clases/30abril.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Imprimir un mensaje en la consola
+            Console.WriteLine("¡Hola, mundo desde C#!");
+        }
+    }
+}
